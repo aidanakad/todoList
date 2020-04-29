f565ebfa228060de405619077c555fa6b29ac2a0
aidanakad <aydana0504@gmail.com>
2020-04-29 20:11:10 +0600

    new branch

--- script.js
+++ script.js
@@ -1,75 +1,82 @@
 const list = document.querySelector('#list')
 
-
-
-const fetchAdd = (body) =>{
-    fetch('http://localhost:3000/add',{
-    method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body:  JSON.stringify(body),
+const createEl = (tag, text, attrs = {})=>{
+    const el = document.createElement(tag)
+    el.textContent = text;
+    Object.keys(attr).forEach((key)=>{
+        el.setAttribute(key, attrs[key])
     })
+    return el
 }
 
-const fetchEdit =(id, body)=>{
-    fetch(`http://localhost:3000/edit/${id}`,{
-    method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-          },
-        body:  JSON.stringify(body),
-    })
+const endpoint = 'http://localhost:3000'
+
+const fetchGetList = ()=>{
+    return fetch(`${endpoint}/list`)
+    .then(response => response.json())
 }
 
-const fetchDelete =(id)=>{
-    fetch(`http://localhost:3000/delete/${id}`,{
-    method: 'DELETE',
+const fetchAddTask = (body) => {
+    return fetch(`${endpoint}/add`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
     })
-}
+  }
+
+  const fetchEditTask = (body, id)=>{
+    return fetch(`${endpoint}/edit/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+  }
+
+  const fetchDeleteTask = (id)=>{
+    return fetch(`${endpoint}/delete/${id}`, {
+        method: 'PUT',
+      })
+  }
 
 
-const renderTask = (task) =>{
-    const li = document.createElement('li');
+  const renderTask = (task)=>{
+      const li = createEl('li')
+      list.appendChild(li)
 
+      const text = createEl('div', task.text)
+      li.appendChild(text)
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Удалить'
+      const btnWrapper = createEl('div', null, {class: 'btn-wrapper'})
+      li.appendChild(btnWrapper)
 
-    deleteBtn.addEventListener('click', () => {
-        fetchDelete(task.id)
-        window.location.reload()
-    })
+      const doneBtnText = !task.done ? 'сделано' : 'не сделано'
 
+      const doneBtn = createEl('button', doneBtnText)
+      const deleteBtn = createEl('button', 'удалить')
+      const editBtn = createEl('button', 'редактировать')
 
-    const isDoneBnt = document.createElement('button')
-    if(task.done === true){
-        isDoneBnt.textContent='Сделано'
-    }else{
-        isDoneBnt.textContent= 'не сделано'
-    }
+      btnWrapper.appendChild(doneBtn)
+      btnWrapper.appendChild(deleteBtn)
+      btnWrapper.appendChild(editBtn)
 
-    isDoneBnt.addEventListener('click', ()=>{
-        fetchEdit(task.id, )
-    })
+      deleteBtn.addEventListener('click', ()=>{
+        fetchDeleteTask(task.id)
+        .then(() => window.location.reload())
+      })
 
+      doneBtn.addEventListener('click', ()=>{
+        fetchEditTask(task.id, { done: !task.done })
+      })
 
+      if(task.done) li.className ('done')
 
-    li.textContent = task.text 
-    li.setAttribute('data-number', task.id);
-
-    if( task.done === true) li.classList.add('done');
-    li.appendChild(deleteBtn)
-    li.appendChild(isDoneBnt)
-    list.appendChild(li)
-}
-
-fetch('http://localhost:3000/list')
-.then(response => {
-    return response.json()
-})
-.then(taskList =>{
-    taskList.forEach(renderTask)
-})
-
-
+      editBtn.addEventListener('click', ()=>{
+        const input = createEl('input')
+        input.type = 'text'
+        input.value = task.text
+        li.replaceChild(input, task.text)
+        input.addEventListener('blur', ()=>{
+          fetchEditTask(task.id, {text: input.value})
+        })
+      })
+  }

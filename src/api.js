6f0fab3aa04eb32adcb6f033b70c318deeaf36b6
aidanakad <aydana0504@gmail.com>
2020-05-05 19:03:43 +0600

    added beautify expansion

--- src/api.js
+++ src/api.js
@@ -1,44 +1,48 @@
 const endpoint = 'http://localhost:3000'
 
 
-export const fetchGetList = ()=>{
+export const fetchGetList = () => {
     return fetch(`${endpoint}/list`)
-    .then(response =>{
-        if (response.ok) return response.json()
-        throw new Error('Не удалось загрузить список задач')
-    })
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Не удалось загрузить список задач')
+        })
 }
 
 export const fetchAddTask = (body) => {
     return fetch(`${endpoint}/add`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    })
-    .then(response =>{
-        if(response.ok) return response.json()
-        throw new Error('Не удалось добавить задачу')
-    })
-  }
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Не удалось добавить задачу')
+        })
+}
 
-export const fetchEditTask = (id, body)=>{
+export const fetchEditTask = (id, body) => {
     return fetch(`${endpoint}/edit/${id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      })
-      .then(response =>{
-          if(response.ok) return response.json()
-          throw new Error('Не удалось внести редактирование')
-      })
-  }
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        })
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Не удалось внести редактирование')
+        })
+}
 
-export  const fetchDeleteTask = (id)=>{
+export const fetchDeleteTask = (id) => {
     return fetch(`${endpoint}/delete/${id}`, {
-        method: 'DELETE',
-      })
-      .then(response=>{
-          if( response.ok) return response.json()
-          throw new Error('Не удалось удалить задачу')
-      })
-  }
+            method: 'DELETE',
+        })
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Не удалось удалить задачу')
+        })
+}
--- src/index.js
+++ src/index.js
@@ -1,74 +1,90 @@
-import {fetchGetList, fetchAddTask, fetchEditTask, fetchDeleteTask} from './api'
-import {createEl} from './tools'
+import {
+    fetchGetList,
+    fetchAddTask,
+    fetchEditTask,
+    fetchDeleteTask
+} from './api'
+import {
+    createEl
+} from './tools'
 
 
 const list = document.querySelector('#list')
 
-    const renderTask = (task) =>{
-        
-        const li = createEl('li')
-        const text = createEl('div', task.text)
-        const btnWrepper = createEl('div')
-        const doneBtnTxt = !task.done ? 'Сделано' : 'Не сделано'
-        const doneBtn = createEl('button', doneBtnTxt)
-        const editBtn = createEl('button', 'Редактировать')
-        const deleteBtn = createEl('button', 'Удалить')
-        list.appendChild(li)
-        li.appendChild(text)
-        li.appendChild(btnWrepper)
-        btnWrepper.appendChild(doneBtn)
-        btnWrepper.appendChild(editBtn)
-        btnWrepper.appendChild(deleteBtn)
+const renderTask = (task) => {
 
-    deleteBtn.addEventListener('click', ()=>{
+    const li = createEl('li')
+    const text = createEl('div', task.text)
+    const btnWrepper = createEl('div')
+    const doneBtnTxt = !task.done ? 'Сделано' : 'Не сделано'
+    const doneBtn = createEl('button', doneBtnTxt)
+    const editBtn = createEl('button', 'Редактировать')
+    const deleteBtn = createEl('button', 'Удалить')
+    list.appendChild(li)
+    li.appendChild(text)
+    li.appendChild(btnWrepper)
+    btnWrepper.appendChild(doneBtn)
+    btnWrepper.appendChild(editBtn)
+    btnWrepper.appendChild(deleteBtn)
+
+    deleteBtn.addEventListener('click', () => {
         fetchDeleteTask(task.id)
-        .then(()=>{
-            li.remove()
-            renderTaskList()
-        })
+            .then(() => {
+                li.remove()
+                renderTaskList()
+            })
     })
 
-    doneBtn.addEventListener('click',() => {
-        fetchEditTask(task.id, {done: !task.done})
-        .then(()=>{
-            li.remove()
-            renderTaskList()
-        })
+    doneBtn.addEventListener('click', () => {
+        fetchEditTask(task.id, {
+                done: !task.done
+            })
+            .then(() => {
+                li.remove()
+                renderTaskList()
+            })
     })
-    if(task.done) li.classList.add('done')
+    if (task.done) li.classList.add('done')
 
-    editBtn.addEventListener('click', () =>{
-        const editInput = createEl('input', {class: 'edit-input'})
+    editBtn.addEventListener('click', () => {
+        const editInput = createEl('input', {
+            class: 'edit-input'
+        })
         editInput.type = 'text'
         editInput.value = task.text
         li.insertBefore(editInput, text)
         li.removeChild(text)
-        editInput.addEventListener('blur', ()=>{
-            fetchEditTask(task.id, {text: editInput.value})
-            .then(()=>{
-                li.remove()
-                renderTaskList()
-            })
+        editInput.addEventListener('blur', () => {
+            fetchEditTask(task.id, {
+                    text: editInput.value
+                })
+                .then(() => {
+                    li.remove()
+                    renderTaskList()
+                })
         })
     })
 }
 
-const renderTaskList = ()=>{
+const renderTaskList = () => {
     const list = document.querySelector('#list')
     fetchGetList()
-    .then(taskList => taskList.forEach((item) => renderTask(item)))
+        .then(taskList => taskList.forEach((item) => renderTask(item)))
 }
- renderTaskList()
+renderTaskList()
 
 const noteArea = document.querySelector('input[name="note"]')
 const textarea = document.querySelector('textarea[name="description"]')
 const createBtn = document.querySelector('#create')
 
-createBtn.addEventListener('click', ()=>{
-  fetchAddTask ({text: noteArea.value, textarea: noteArea.value})
-  .then(()=>{
-      const list = document.querySelector('#list')
-      list.remove()
-      renderTaskList()
-  })
+createBtn.addEventListener('click', () => {
+    fetchAddTask({
+            text: noteArea.value,
+            textarea: noteArea.value
+        })
+        .then(() => {
+            const list = document.querySelector('#list')
+            list.remove()
+            renderTaskList()
+        })
 })
